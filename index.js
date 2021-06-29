616808b0a24a6186da88865520a0d560146c6262
Andrew Masih <armasih@outlook.com>
2021-06-29 00:46:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/* The 'break' is also useful in GAME development when the user wants to continue playing until a certain condition is met, eg quitting the game. In that way you couldwrite an infinite loop (eg while (true) ) and only break after the user takes some action
+
+Challenge.
+
+1. write a for loop that iterates throght the numbers 0-10 and logs them to the console.
+
+2. break the loop after logginh the number 5
+
+*/
+
+let i = 0
+
+
+// i wrote the loop here
+for (i = 0; 1 < 10; i ++) {
+
+  if (i === 5) {
+
+    break;
+  }
+   
+  console.log(i);
+}
